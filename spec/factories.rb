ef878d650790b8d4d62b9139bc7f899d4f433920
leninvoctyabre <shevchuku@gmail.com>
2011-05-06 09:43:27 +0300

    Модель User с паролями и страницей показывающей пользователя

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# Используя символ ':user', мы говорим Factory Girl симулировать модель User.
+Factory.define :user do |user|
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
